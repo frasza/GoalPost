9efd35ec5a0a92504dbcb85f14508abc5a3e32a3
Zan Fras <zan.fraas@gmail.com>
2018-03-25 13:07:52 +0200

    create bindtokeyboard extension + button color extension

--- GoalPost/Controller/CreateGoalViewController.swift
+++ GoalPost/Controller/CreateGoalViewController.swift
@@ -17,9 +17,16 @@ class CreateGoalViewController: UIViewController {
     @IBOutlet weak var longTermButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    //MARK: - Properties
+    /***************************************************************/
+    var goalType: GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     //MARK: - Actions
@@ -29,11 +36,15 @@ class CreateGoalViewController: UIViewController {
     }
     
     @IBAction func shortTermButtonPressed(_ sender: Any) {
-        
+        goalType = .shortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     @IBAction func longTermButtonPressed(_ sender: Any) {
-        
+        goalType = .longTerm
+        longTermButton.setSelectedColor()
+        shortTermButton.setDeselectedColor()
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
